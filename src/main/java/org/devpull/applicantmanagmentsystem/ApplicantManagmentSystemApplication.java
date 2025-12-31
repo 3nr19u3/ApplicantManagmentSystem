5cfcee2cd6c33f5e97581b37627f39af0c3227d5
Luis Gutierrez <kike123kob@gmail.com>
2025-12-31 01:44:57 -0500

    initial commit

---
+++ src/main/java/org/devpull/applicantmanagmentsystem/ApplicantManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package org.devpull.applicantmanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicantManagmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicantManagmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/devpull/applicantmanagmentsystem/ApplicantManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devpull.applicantmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicantManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
